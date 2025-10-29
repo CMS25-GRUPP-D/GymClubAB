aa4c6fbe0acbae2fdaf53e4ad353cd73a323ec86
ihsnack <ihsnack@fastmail.com>
2025-10-29 09:32:22 +0100

    Add project files.

---
+++ Infrastructure/Interfaces/IMemberService.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface IMemberService
+{
+    Task<bool> SaveMemberAsync(Member member);
+    Task<bool> UpdateMemberAsync(Member member);
+    Task<bool> DeleteMemberAsync(string id);
+    Task<Member> GetMemberByIdAsync(string id);
+    Task<IEnumerable<Member>> GetAllMemberAsync();
+}
---
+++ Infrastructure/Services/MemberService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace Infrastructure.Services
+{
+    public class MemberService : IMemberService
+    {
+        public Task<bool> DeleteMemberAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Member>> GetAllMemberAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Member> GetMemberByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveMemberAsync(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateMemberAsync(Member member)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
