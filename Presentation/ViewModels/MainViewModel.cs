d00cc22874712f7ec9a173ca0c6d7a812878a496
AlayaStHill <alaya.st.hill@gmail.com>
2025-10-30 12:54:33 +0100

    Add MainViewModel-class to Presentation

---
+++ Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Presentation.ViewModels;
+
+public class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableObject _currentViewModel = null!;
+}
