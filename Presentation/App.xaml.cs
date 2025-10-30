be93201fb45aad4cdec0e4364f93ce540b34ce92
Berrin <Berrin>
2025-10-30 14:24:14 +0100

    Add Dependency Injection i App.xaml, remove startupuri

--- Presentation/App.xaml.cs
+++ Presentation/App.xaml.cs
@@ -1,14 +1,43 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Presentation.ViewModels;
+using Presentation.Views;
 
-namespace Presentation
+namespace Presentation;
+
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private IHost _host;
+
+    public App()
     {
+        _host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MainViewModel>();
+            services.AddTransient<MemberAddViewModel>();
+            services.AddTransient<MemberAddView>();
+
+        })
+   
+            .Build();
+
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        MainViewModel mainViewModel = _host!.Services.GetRequiredService<MainViewModel>();
+        mainViewModel.CurrentViewModel = _host!.Services.GetRequiredService<MemberAddViewModel>();
+
+        MainWindow mainWindow = _host!.Services.GetRequiredService<MainWindow>();
+        mainWindow.DataContext = mainViewModel;
+
+        mainWindow.Show();
+    }
 }
