0a80e786a443ad5f9026a2f8b48aa3ce0e36bea7
Alexander <113015594+Alexware0211@users.noreply.github.com>
2025-10-31 15:09:28 +0100

    Create Member Format WPF (#11)

--- Presentation/ViewModels/MemberAddViewModel.cs
+++ Presentation/ViewModels/MemberAddViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 
 namespace Presentation.ViewModels;
 
 public partial class MemberAddViewModel : ObservableObject
 {
+    [ObservableProperty]
+
+    private string _title = "Register Member";
+
 
 }
+
