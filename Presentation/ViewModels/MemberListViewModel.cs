a7a6c2d3959fb788be358be06bb058783d5e3541
AlayaStHill <alaya.st.hill@gmail.com>
2025-11-03 15:46:58 +0100

    Add member list UI and scaffold MemberListViewModel commands
    - Create MemberListView with ListView bound to Members showing Member fields
    - Add “New member” button bound to GoToMemberAddViewCommand
    - Add Edit and Delete buttons bound to EditCommand and DeleteCommand
    - Bind Title and SuccessMessage to header and status TextBlocks
    - Introduce observable properties in MemberListViewModel: Title, Members, SuccessMessage
    - Inject IServiceProvider and IMemberService into MemberListViewModel
    - Add RelayCommand: PopulateMemberList, GoToMemberAddView, Edit, Delete

--- Presentation/ViewModels/MemberListViewModel.cs
+++ Presentation/ViewModels/MemberListViewModel.cs
@@ -1,5 +1,57 @@
-﻿namespace Presentation.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 
-public class MemberListViewModel
+namespace Presentation.ViewModels;
+
+public partial class MemberListViewModel : ObservableObject
 {
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IMemberService _memberService;
+
+    public MemberListViewModel(IServiceProvider serviceProvider, IMemberService memberService)
+    {
+        _serviceProvider = serviceProvider;
+        _memberService = memberService;
+
+        //Addera PopulateMemberList
+    }
+
+    [ObservableProperty]
+    private string _title = "Member list";
+
+    [ObservableProperty]
+    private ObservableCollection<Member> _members = [];
+
+    [ObservableProperty]
+    private string _successMessage = null!;
+
+    [RelayCommand]
+    private void PopulateMemberList()
+    {
+
+    }
+
+    [RelayCommand]
+    private void GoToMemberAddView()
+    {
+
+    }
+
+    [RelayCommand]
+    private void Edit(Member member)
+    {
+
+    }
+
+    [RelayCommand]
+    private void Delete(Member member)
+    {
+
+    }
+
 }
