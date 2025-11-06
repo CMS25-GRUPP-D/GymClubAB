39c720d4352d2fdc60f25d71977a0a206ed95dd7
ihsnack <ihsnack@fastmail.com>
2025-11-06 11:40:05 +0100

    clean up code

--- Infrastructure/Services/MemberService.cs
+++ Infrastructure/Services/MemberService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Pipelines;
-using System.Linq;
-using System.Runtime.Intrinsics.X86;
-using System.Text;
-using System.Threading.Tasks;
-using Infrastructure.Interfaces;
+﻿using Infrastructure.Interfaces;
 using Infrastructure.Models;
 
 namespace Infrastructure.Services
