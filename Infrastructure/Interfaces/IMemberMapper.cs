07d3a3114f7ae0d759247df9e9942b4ecd490899
AlayaStHill <alaya.st.hill@gmail.com>
2025-11-07 14:09:52 +0100

    Add MemberMapper to be able to map member to memberupdaterequest and vice versa. Fix method-signatures IMemberMapper

--- Infrastructure/Interfaces/IMemberMapper.cs
+++ Infrastructure/Interfaces/IMemberMapper.cs
@@ -1,10 +1,11 @@
 ﻿using Infrastructure.DTOs;
+using Infrastructure.Models;
 
 namespace Infrastructure.Interfaces;
 public interface IMemberMapper
 {
     // member -> memberpdaterequest (edit-metod listviewmodel)
-    MemberUpdateRequest MapFromMemberToUpdateRequest(MemberUpdateRequest member);
+    MemberUpdateRequest MapFromMemberToUpdateRequest(Member member);
     // memberupdaterequest -> member (UpdateMemberAsync memberservice)
-    MemberUpdateRequest MapFromUpdateRequestToMember(MemberUpdateRequest updateRequest);
+    Member MapFromUpdateRequestToMember(MemberUpdateRequest updateRequest);
 }
---
+++ Infrastructure/Mappers/MemberMapper.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.DTOs;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace Infrastructure.Mappers;
+
+public class MemberMapper : IMemberMapper
+{
+    public MemberUpdateRequest MapFromMemberToUpdateRequest(Member member)
+    {
+        MemberUpdateRequest updateRequest = new()
+        {
+            SocialSecurityNumber = member.SocialSecurityNumber,
+            FirstName = member.FirstName,
+            LastName = member.LastName,
+            Email = member.Email,
+            Phonenumber = member.Phonenumber,
+            Membership = member.Membership
+        };
+
+        return updateRequest;
+    }
+
+    public Member MapFromUpdateRequestToMember(MemberUpdateRequest updateRequest)
+    {
+        Member member = new Member
+        {
+            SocialSecurityNumber = updateRequest.SocialSecurityNumber,
+            FirstName = updateRequest.FirstName,
+            LastName = updateRequest.LastName,
+            Email = updateRequest.Email,
+            Phonenumber = updateRequest.Phonenumber,
+            Membership = updateRequest.Membership
+        };
+
+        return member;
+    }
+}
