dea8e6e7841420e0fcf569d532566e01230c9d36
AlayaStHill <alaya.st.hill@gmail.com>
2025-11-07 14:02:20 +0100

    Add IMemberMapper

---
+++ Infrastructure/Interfaces/IMemberMapper.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.DTOs;
+
+namespace Infrastructure.Interfaces;
+public interface IMemberMapper
+{
+    // member -> memberpdaterequest (edit-metod listviewmodel)
+    MemberUpdateRequest MapFromMemberToUpdateRequest(MemberUpdateRequest member);
+    // memberupdaterequest -> member (UpdateMemberAsync memberservice)
+    MemberUpdateRequest MapFromUpdateRequestToMember(MemberUpdateRequest updateRequest);
+}
